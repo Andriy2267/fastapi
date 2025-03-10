f74126a0241e615c233abb9ddc9e880694034cab
Andriy2267 <andrijcopek696@gmail.com>
2025-03-10 19:43:39 +0200

    User table and some code changes have been added to the project

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from pydantic import BaseModel
 from random import randrange
+from typing import List
 from psycopg2.extras import RealDictCursor
 import psycopg2
 import time
 from . import models
 from .database import engine, get_db
+from . import schemas
 from sqlalchemy.orm import Session
 
 
@@ -13,11 +14,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
 
 while True:
     try:
@@ -36,31 +32,31 @@ async def root():
     return {"message": "Hello"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
     all_posts = db.query(models.Post).all()
 
-    return {"data": all_posts}
+    return all_posts
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def post_info(post: Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
 
-    return {"your post": new_post}
+    return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 async def get_post(id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"This id {id} was not found")
-    return {"Your post": post}
+    return post
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -77,8 +73,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
-def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     post_quary = db.query(models.Post).filter(models.Post.id == id)
     post = post_quary.first()
 
@@ -89,4 +85,15 @@ def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
     post_quary.update(updated_post.dict(), synchronize_session=False)
     db.commit()
 
-    return {"data": post_quary.first()}
+    return post_quary.first()
+
+
+@app.post("/user", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
