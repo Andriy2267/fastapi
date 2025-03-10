e29b25804d612293249da703d167ed6ef1f35b11
Andriy2267 <andrijcopek696@gmail.com>
2025-03-10 13:41:44 +0200

    Updated and сlarified code

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
-# C:\Users\ASUS\PycharmProjects
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from pydantic import BaseModel
 from random import randrange
 from psycopg2.extras import RealDictCursor
 import psycopg2
 import time
+from . import models
+from .database import engine, get_db
+from sqlalchemy.orm import Session
 
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 class Post(BaseModel):
     title: str
@@ -26,46 +31,32 @@ while True:
         time.sleep(2)
 
 
-my_post = [
-    {"title": "My 1 title", "content": "My 1 content", "id": 1},
-    {"title": "My 2 title", "content": "My 2 content", "id": 2}
-]
-
-# Function to find a post by ID
-def find_post(id):
-    for p in my_post:
-        if p["id"] == id:
-            return p
-
-async def find_index_post(id):
-    for k, v in enumerate(my_post):
-        if v["id"] == id:
-            return k
-
 @app.get("/")
 async def root():
     return {"message": "Hello"}
 
+
 @app.get("/posts")
-async def get_posts():
-    cursor.execute("""SELECT * FROM posts """)
-    posts = cursor.fetchall()
-    return {"data": posts}
+async def get_posts(db: Session = Depends(get_db)):
+    all_posts = db.query(models.Post).all()
+
+    return {"data": all_posts}
+
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def post_info(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+async def post_info(post: Post, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
 
     return {"your post": new_post}
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id)))
-    post = cursor.fetchone()
+async def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"This id {id} was not found")
@@ -73,27 +64,29 @@ def get_post(id: int):
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
 
-    if deleted_post == None:
+    if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="message: " + f"such id {id} was not found")
+    
+    post.delete(synchronize_session=False)
+    db.commit()
     
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
-async def update_post(id: int, post: Post):
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
-                   (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    post_quary = db.query(models.Post).filter(models.Post.id == id)
+    post = post_quary.first()
 
-    if updated_post == None:
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="message" + f"such {id} id was not found")
+    
+    post_quary.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
 
-    return {"message": updated_post}
+    return {"data": post_quary.first()}
