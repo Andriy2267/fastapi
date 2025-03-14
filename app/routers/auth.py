756ed11ab888d9963ec26466b7e216ed703670da
Andriy2267 <andrijcopek696@gmail.com>
2025-03-14 20:06:09 +0200

    Authentication has been added to the project

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -7,15 +7,15 @@ router = APIRouter(
     tags = ["Authentication"]
 )
 
-@router.post("/login")
+@router.post("/login", response_model=schemas.Token)
 async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     login_user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not login_user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No such credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"No such credentials")
     
     if not utils.verify(user_credentials.password, login_user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No such credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"No such credentials")
     
     # create token
     # return token
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from ..database import engine, get_db
-from .. import schemas, models
+from .. import schemas, models, oauth2
 from sqlalchemy.orm import Session
 from typing import List
 
@@ -10,14 +10,16 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-async def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     all_posts = db.query(models.Post).all()
 
     return all_posts
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),
+                      user_id: int = Depends(oauth2.get_current_user)):
+    print(user_id)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -27,7 +29,7 @@ async def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/{id}", response_model=schemas.Post)
-async def get_post(id: int, db: Session = Depends(get_db)):
+async def get_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
@@ -37,7 +39,8 @@ async def get_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db),
+                user_id: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
 
     if post.first() == None:
@@ -51,7 +54,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db),
+                user_id: int = Depends(oauth2.get_current_user)):
     post_quary = db.query(models.Post).filter(models.Post.id == id)
     post = post_quary.first()
 
