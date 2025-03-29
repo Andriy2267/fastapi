37b8aa43d0c74f4eb92e470180ad992fece74d8d
Andriy2267 <andrijcopek696@gmail.com>
2025-03-29 18:42:58 +0200

    Fix env and port issues

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ from .database import engine
 from .routers import post, user, auth, vote
 from .config import settings
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 # models.Base.metadata.create_all(bind=engine)
 
@@ -31,4 +32,8 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
